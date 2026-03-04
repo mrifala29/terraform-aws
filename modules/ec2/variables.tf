aa2a3b5cb80f5673ca821b4c88c7a9e87a84a106
mrifala29 <mrifala29@gmail.com>
2026-03-05 02:08:21 +0700

    Update final modules

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "subnet_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
---
+++ modules/vpc/main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "dev-vpc"
+  }
+}
+
+resource "aws_subnet" "this" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.subnet_cidr
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "dev-subnet"
+  }
+}
